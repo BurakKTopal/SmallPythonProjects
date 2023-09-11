399bed6138c9774ed6d960afaa6794d62d0b69ca
Burak Kucuktopal <131784330+BurakKTopal@users.noreply.github.com>
2023-09-11 11:46:00 +0200

    Update Helper.py

--- Pong/PongAI/VisualsAndPlotting/Helper.py
+++ Pong/PongAI/VisualsAndPlotting/Helper.py
@@ -3,17 +3,22 @@ from IPython import display
 
 plt.ion()
 
-def plot(scores, mean_scores):
+def plot(scores_1, mean_scores_1, scores_2, mean_scores_2):
     display.clear_output(wait=True)
     display.display(plt.gcf())
     plt.clf()
     plt.title('Training...')
     plt.xlabel('Number of Games')
     plt.ylabel('Score')
-    plt.plot(scores)
-    plt.plot(mean_scores)
+    #plt.plot(scores_1, label="score AI P1")  # for plotting the score of AI Player One
+    plt.plot(mean_scores_1, label="mean score AI P1")
+    #plt.plot(scores_2, label="score AI P2")  # for plotting the score of AI Player Two
+    plt.plot(mean_scores_2, label="mean score AI P2") 
     plt.ylim(ymin=0)
-    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
-    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    if mean_scores_1:
+        plt.text(len(mean_scores_1)-1, mean_scores_1[-1], str(mean_scores_1[-1]))
+    if mean_scores_2:
+        plt.text(len(mean_scores_2)-1, mean_scores_2[-1], str(mean_scores_2[-1]))
     plt.show(block=False)
+    plt.legend(loc='upper left')
     plt.pause(.1)
