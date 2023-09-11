291c8fa6c9fbad6aabe98f0e8ddb7322c8061aa9
Burak Kucuktopal <131784330+BurakKTopal@users.noreply.github.com>
2023-09-11 12:38:14 +0200

    Update Main.py

--- Pong/Main.py
+++ Pong/Main.py
@@ -5,8 +5,8 @@ HEIGHT = 400
 
 MAX_FPS = 10
 IMAGES = {}
-IMAGES["Board_1"] = p.transform.scale(p.image.load("PongAI/VisualsAndPlotting/IMAGES/Board_1_black.png"), (120, 10))
-IMAGES["Board_2"] = p.transform.scale(p.image.load("PongAI/VisualsAndPlotting/IMAGES/Board_2_black.png"), (120, 10))
+IMAGES["Board_1"] = p.transform.scale(p.image.load("PongAI/VisualsAndPlotting/IMAGES/Board_1_white.png"), (120, 10))
+IMAGES["Board_2"] = p.transform.scale(p.image.load("PongAI/VisualsAndPlotting/IMAGES/Board_2_white.png"), (120, 10))
 
 gamestate = Game(WIDTH, HEIGHT)
 
@@ -55,9 +55,10 @@ p.quit()
 
 
 def drawGameState(screen, gamestate):
-    screen.fill(p.Color("white"))
+    screen.fill(p.Color("black"))
     drawBoards(screen, gamestate)
     drawBall(screen, gamestate)
+    drawDashedLine(screen)
 
 def drawBoards(screen, gamestate):
     board_one = IMAGES["Board_1"].get_rect()
@@ -74,8 +75,28 @@ def drawBoards(screen, gamestate):
 
 
 def drawBall(screen, gamestate):
-    p.draw.circle(screen, "red", (gamestate.ball[0], gamestate.ball[1]), 7)
+    p.draw.circle(screen, "white", (gamestate.ball[0], gamestate.ball[1]), 5)
     return
+
+def drawDashedLine(screen):
+    """"
+    Adding dashed line in the middle
+    """
+
+    dash_length = 20
+    gap_length = 10
+    line_color = "white"
+    line_width = 5
+
+    start_x = 0
+    end_x = WIDTH
+    y = HEIGHT // 2
+
+    x = start_x
+
+    while x < end_x:
+        p.draw.line(screen, line_color, (x, y), (x + dash_length, y), line_width)
+        x += dash_length + gap_length
 
 
 if __name__ == '__main__':
